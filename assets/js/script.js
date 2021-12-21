b905e8306cf92ffcb9c2c729ff9c627df2efad97
Kratu Desai <kratudesai@kratus-macbook-pro-2.local>
2021-12-21 20:20:55 +0000

    general update on green and blue, hue, saturation and lighness part added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,14 @@ $(document).on('input', '.red-range', function () {
 });
 $(document).on('input', '.green-range', function () {
 	greenRange = $(this).val();
+	$(".greenNumber").text(greenRange);
+	$(".greenPercent").text(Math.floor(greenRange*100/255));
 	bColor();
 });
 $(document).on('input', '.blue-range', function () {
 	blueRange = $(this).val();
+	$(".blueNumber").text(blueRange);
+	$(".bluePercent").text(Math.floor(blueRange*100/255));
 	bColor();
 });
 
@@ -40,16 +44,16 @@ const gradientColor = (r,g,b) => {
 	$('.color1').css('background-color', `rgb(${redRange},0,0)`);
 	$('.color2').css('background-color', `rgb(0,${greenRange},0)`);
 	$('.color3').css('background-color', `rgb(0,0,${blueRange})`);
-	$('.color-2').css('background-color', `rgba(${r},${g},${b},0.1)`);
-	$('.color-3').css('background-color', `rgba(${r},${g},${b},0.2)`);
-	$('.color-4').css('background-color', `rgba(${r},${g},${b},0.3)`);
-	$('.color-5').css('background-color', `rgba(${r},${g},${b},0.4)`);
-	$('.color-6').css('background-color', `rgba(${r},${g},${b},0.5)`);
-	$('.color-7').css('background-color', `rgba(${r},${g},${b},0.6)`);
-	$('.color-8').css('background-color', `rgba(${r},${g},${b},0.7)`);
-	$('.color-9').css('background-color', `rgba(${r},${g},${b},0.8)`);
-	$('.color-10').css('background-color', `rgba(${r},${g},${b},0.9)`);
-	$('.color-11').css('background-color', `rgba(${r},${g},${b},1)`);
+	$('.color-2').css('background-color', `rgba(${r},${g},${b},0.1)`).text("10%");
+	$('.color-3').css('background-color', `rgba(${r},${g},${b},0.2)`).text("20%");
+	$('.color-4').css('background-color', `rgba(${r},${g},${b},0.3)`).text("30%");
+	$('.color-5').css('background-color', `rgba(${r},${g},${b},0.4)`).text("40%");
+	$('.color-6').css('background-color', `rgba(${r},${g},${b},0.5)`).text("50%");
+	$('.color-7').css('background-color', `rgba(${r},${g},${b},0.6)`).text("60%");
+	$('.color-8').css('background-color', `rgba(${r},${g},${b},0.7)`).text("70%");
+	$('.color-9').css('background-color', `rgba(${r},${g},${b},0.8)`).text("80%");
+	$('.color-10').css('background-color', `rgba(${r},${g},${b},0.9)`).text("90%");
+	$('.color-11').css('background-color', `rgba(${r},${g},${b},1)`).text("100%");
 }
 
 bColor();
